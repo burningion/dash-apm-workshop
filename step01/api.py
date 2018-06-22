f522918e7ff653a582406ce9c7a3f34e6573c7bc
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2018-06-22 16:44:08 -0400

    Don't send headers in step 01

--- step01/api.py
+++ step01/api.py
@@ -12,17 +12,13 @@ from ddtrace.contrib.flask import TraceMiddleware
 # Tracer configuration
 tracer.configure(hostname='agent')
 
-
 app = Flask('api')
 traced_app = TraceMiddleware(app, tracer, service='thinker-api')
 
 
 @app.route('/think/')
 def think_handler():
-    thoughts = requests.get('http://thinker:5001/', headers={
-        'x-datadog-trace-id': str(tracer.current_span().trace_id),
-        'x-datadog-parent-id': str(tracer.current_span().span_id),
-    }, params={
+    thoughts = requests.get('http://thinker:5001/', params={
         'subject': flask_request.args.getlist('subject', str),
     }).content
     return Response(thoughts, mimetype='application/json')
