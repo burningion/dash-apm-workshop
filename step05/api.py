6cfc56ba4bf10ae59b126a00e0d93045e0748057
KP Kaiser <kirk@zothcorp.com>
2018-07-03 09:55:57 -0400

    Add Flask-Cache for redis in step05

---
+++ step05/api.py
@@ -0,0 +1,28 @@
+import blinker as _
+import requests
+
+from flask import Flask, Response
+from flask import jsonify
+from flask import request as flask_request
+
+from ddtrace import tracer, patch
+from ddtrace.contrib.flask import TraceMiddleware
+
+
+# Tracer configuration
+tracer.configure(hostname='agent')
+patch(requests=True)
+
+app = Flask('api')
+traced_app = TraceMiddleware(app, tracer, service='thinker-api')
+
+
+@app.route('/think/')
+def think_handler():
+    thoughts = requests.get('http://thinker:5001/', headers={
+        'x-datadog-trace-id': str(tracer.current_span().trace_id),
+        'x-datadog-parent-id': str(tracer.current_span().span_id),
+    }, params={
+        'subject': flask_request.args.getlist('subject', str),
+    }).text
+    return Response(thoughts, mimetype='application/json')
---
+++ step05/thinker.py
@@ -0,0 +1,36 @@
+import blinker as _
+import requests
+
+from flask import Flask, Response, jsonify
+from flask import jsonify
+from flask import request as flask_request
+
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+from bootstrap import create_app
+from models import Thought
+
+from time import sleep
+
+app = create_app()
+traced_app = TraceMiddleware(app, tracer, service='thinker-microservice', distributed_tracing=True)
+
+# Tracer configuration
+tracer.configure(hostname='agent')
+
+@tracer.wrap(name='think')
+def think(subject):
+    tracer.current_span().set_tag('subject', subject)
+
+    sleep(0.5)
+    quote = Thought.query.filter_by(subject=subject).first()
+    
+    return quote
+
+@app.route('/')
+def think_microservice():
+    # because we have distributed tracing, don't need to manually grab headers
+    subject = flask_request.args.get('subject')
+    thoughts = think(subject)
+    return jsonify(thoughts.serialize())
